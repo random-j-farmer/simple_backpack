276982d11b7cb46e5d62005075844c7b9b5bcbd7
kwpugh <kwpugh@192.168.68.54>
2020-08-12 18:36:10 -0500

    updates

--- remappedSrc/com/kwpugh/simple_backpack/backpack/BackpackScreenHandler.java
+++ remappedSrc/com/kwpugh/simple_backpack/backpack/BackpackScreenHandler.java
@@ -33,17 +33,15 @@ public class BackpackScreenHandler extends ScreenHandler
         inventory.onOpen(playerInventory.player);
         setupSlots(false);
     }
-
-    
-    
+   
 	@Override
 	public ItemStack onSlotClick(int slotId, int clickData, SlotActionType actionType, PlayerEntity playerEntity)
 	{	
 		if (slotId >= 0) // slotId < 0 are used for networking internals, to avoid array out of range (-999)
 		{
 			ItemStack stack = getSlot(slotId).getStack();
-			
-			if((stack.getItem() instanceof BackpackItem)  ||  (actionType == SlotActionType.SWAP || actionType == SlotActionType.QUICK_MOVE))
+
+			if((stack.getItem() instanceof BackpackItem)  ||  (actionType == SlotActionType.SWAP))
 			{
 				// Prevent moving bags around
 				return stack;
