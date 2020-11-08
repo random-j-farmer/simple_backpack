f75a40458c67b151e724b688444109df9e2c5d12
kwpugh <kwpugh@192.168.68.55>
2020-11-08 14:25:36 -0600

    updates

--- src/main/java/com/kwpugh/simple_backpack/BackpackClient.java
+++ src/main/java/com/kwpugh/simple_backpack/BackpackClient.java
@@ -18,7 +18,7 @@ public class BackpackClient implements ClientModInitializer
      @Override
     public void onInitializeClient()
     {
-        ScreenProviderRegistry.INSTANCE.<BackpackScreenHandler>registerFactory(Backpack.BACKPACK_IDENTIFIER, (container -> new BackpackClientScreen(container, MinecraftClient.getInstance().player.inventory, new TranslatableText(Backpack.BACKPACK_TRANSLATION_KEY))));
-        ScreenProviderRegistry.INSTANCE.<VoidPackScreenHandler>registerFactory(Backpack.VOID_PACK_IDENTIFIER, (container -> new VoidPackClientScreen(container, MinecraftClient.getInstance().player.inventory, new TranslatableText(Backpack.VOID_PACK_TRANSLATION_KEY))));
+        ScreenProviderRegistry.INSTANCE.<BackpackScreenHandler>registerFactory(Backpack.BACKPACK_IDENTIFIER, (container -> new BackpackClientScreen(container, MinecraftClient.getInstance().player.getInventory(), new TranslatableText(Backpack.BACKPACK_TRANSLATION_KEY))));
+        ScreenProviderRegistry.INSTANCE.<VoidPackScreenHandler>registerFactory(Backpack.VOID_PACK_IDENTIFIER, (container -> new VoidPackClientScreen(container, MinecraftClient.getInstance().player.getInventory(), new TranslatableText(Backpack.VOID_PACK_TRANSLATION_KEY))));
     }
 }
